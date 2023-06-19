c4fe6b30951e258058fa093637c196afad0d6fa3
rich-base <rich.base521@gmail.com>
2023-06-19 23:56:59 +0900

    Http要求受信機能追加

--- HttpServerTest/Form2.cs
+++ HttpServerTest/Form2.cs
@@ -5,22 +5,20 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace HttpServerTest
 {
     public partial class Form2 : Form
     {
-        private System.Net.HttpListener httpListener;
-        private int counter1;
-        private int counter2;
+        private HttpListener httpListener;
+        delegate void AppendTextDelegate(string text);
         public Form2()
         {
             InitializeComponent();
-            counter1 = 0;
-            counter2 = 0;
-            httpListener = new System.Net.HttpListener();
+            
         }
 
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
@@ -31,21 +29,70 @@ namespace HttpServerTest
 
         private void start_Click(object sender, EventArgs e)
         {
-            logTextBox.Text = "START Pushed. " + counter1 + "\r\n" + logTextBox.Text;
-            counter1++;
+            logTextBox.AppendText("START.\r\n");
+            try
+            {
+                logTextBox.AppendText("http://localhost:8080/\r\n");
+                Thread t = new Thread(new ThreadStart(threadServer));
+                t.Start();
+            }
+            catch (Exception except)
+            {
+                logTextBox.AppendText(except.Message);
+            }
         }
 
         private void stop_Click(object sender, EventArgs e)
         {
-            logTextBox.Text = "STOP Pushed. " + counter2 + "\r\n" + logTextBox.Text;
-            counter2++;
+            logTextBox.AppendText("STOP.\r\n");
+            if(httpListener != null)
+            {
+                httpListener.Stop();
+            }
         }
 
         private void clear_Click(object sender, EventArgs e)
         {
             logTextBox.Clear();
-            counter1 = 0;
-            counter2 = 0;
+        }
+
+        public void threadServer()
+        {
+            try
+            {
+                if (httpListener == null)
+                {
+                    httpListener = new System.Net.HttpListener();
+                    httpListener.Prefixes.Clear();
+                    httpListener.Prefixes.Add(@"http://+:8080/");
+                    //この設定でサーバ起動するためには管理者権限で以下のコマンドを実行しておく必要がある
+                    //netsh http add urlacl url=http://+:8080/ user=Everyone
+                }
+                httpListener.Start();
+                while (true)
+                {
+                    HttpListenerContext context = httpListener.GetContext();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+                    if (request != null)
+                    {
+                        string query = request.Url.Query;
+                        Invoke(new AppendTextDelegate(logTextBox.AppendText), query + "\r\n");
+                        byte[] text = Encoding.UTF8.GetBytes(query);
+                        response.OutputStream.Write(text, 0, text.Length);
+                    }
+                    else
+                    {
+                        response.StatusCode = 404;
+                    }
+                    response.Close();
+
+                }
+            }
+            catch(Exception except)
+            {
+                Invoke(new AppendTextDelegate(logTextBox.AppendText), except.Message);
+            }
         }
     }
 }
